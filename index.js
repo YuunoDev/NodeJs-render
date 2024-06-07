4579168440d34e05fe5f2578e7ce54d5a7b0e307
YuunoDev <adrianalonso.a4@proton.me>
2024-06-07 09:06:52 -0600

    chore: Update route path for calculations endpoint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const misrutas = require('./routes/rutas');
+const misrutas = require('./routes/rutas.js');
 
 const app = express();
 const port = process.env.PORT || 3000;
